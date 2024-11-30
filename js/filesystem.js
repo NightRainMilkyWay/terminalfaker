acc988fee500800a1ac261064fb3a0ef95694201
夜雨 星河 <nightrainmilkyway@gmail.com>
2024-11-30 21:30:23 +0800

    just for fun

--- js/filesystem.js
+++ js/filesystem.js
@@ -1,38 +1,7 @@
 var originalFilesystem="<d name='/' path='/'>\
             <c>\
-            <d name='docs' path='/docs/'>\
-                <c>\
-                    <d name='private' path='/docs/private/'>\
-                        <c>\
-                            <f name='secret.txt' path='/docs/private/'>\
-                                <contents>PxNmGkl6M+jDP4AYAKZET18SEnWD5qw5LIP9174lONWslF144K9VHFIk1JA=</contents>\
-                            </f>\
-                        </c>\
-                    </d>\
-                    <f name='shoplist.txt' path='/docs/'>\
-                        <contents>-Apples\n-Bananas\n-Cookies</contents>\
-                    </f>\
-                    <f name='ok.txt' path='/docs/'>\
-                        <contents>I am ok.</contents>\
-                    </f>\
-                    <f name='moretodo.txt' path='/docs/'>\
-                        <contents>A, B, C.</contents>\
-                    </f>\
-                </c>\
-            </d>\
-            <d name='more' path='/more/'>\
-                <c>\
-                    <f name='moretodo.txt' path='/more/'>\
-                        <contents>Don't forget this other stuff.</contents>\
-                    </f>\
-                </c>\
-            </d>\
-            <d name='stuff' path='/stuff/'>\
-                <c>\
-                </c>\
-            </d>\
-            <f name='cool.txt' path='/'>\
-                <contents>There is a hidden command in this terminal called 'secret'.</contents>\
+            <f name='docs.md' path='/'>\
+                <contents>performance</contents>\
             </f>\
             </c>\
         </d>";
--- js/src/boot.js
+++ js/src/boot.js
@@ -1,8 +1,29 @@
-var bootMessageLines = [
-            "System loading...<br>",
-            (new Date()).toString()+"<br>",
-            "&nbsp;_______&nbsp;&nbsp;&nbsp;______&nbsp;<br>|__&nbsp;&nbsp;&nbsp;__|&nbsp;|&nbsp;&nbsp;____|<br>&nbsp;&nbsp;&nbsp;|&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;|__&nbsp;&nbsp;&nbsp;<br>&nbsp;&nbsp;&nbsp;|&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;__|&nbsp;&nbsp;<br>&nbsp;&nbsp;&nbsp;|&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>&nbsp;&nbsp;&nbsp;|_|&nbsp;&nbsp;&nbsp;&nbsp;|_|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>",
-            "Terminal Faker version "+version+"<br>",
-            "By Sherri Wheeler (SyntaxSeed.com)<br>",
-            "Ready. Type 'help' to get started.<br><br>"
-        ];
+const browserLang = navigator.language || navigator.userLanguage;
+const zhLangs = ["zh-CN", "zh-TW", "zh-HK", "zh"];
+let isZH = zhLangs.some((lang) => browserLang.startsWith(lang));
+
+var bootMessageLines_en = [
+  "System loading...<br>",
+  new Date().toString() + "<br>",
+  "&nbsp;_______&nbsp;&nbsp;&nbsp;______&nbsp;<br>|__&nbsp;&nbsp;&nbsp;__|&nbsp;|&nbsp;&nbsp;____|<br>&nbsp;&nbsp;&nbsp;|&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;|__&nbsp;&nbsp;&nbsp;<br>&nbsp;&nbsp;&nbsp;|&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;__|&nbsp;&nbsp;<br>&nbsp;&nbsp;&nbsp;|&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>&nbsp;&nbsp;&nbsp;|_|&nbsp;&nbsp;&nbsp;&nbsp;|_|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>",
+  "Terminal Faker version " + version + "<br>",
+  "By Sherri Wheeler (SyntaxSeed.com)<br>",
+  "Ready. Type 'help' to get started.<br><br>",
+  "topic:<br>",
+  "There is a docs.md file in the current directory. Please print it as a verification for joining the group.<br><br>",
+];
+
+
+var bootMessageLines_zh = [
+  "System loading...<br>",
+  new Date().toString() + "<br>",
+  "&nbsp;_______&nbsp;&nbsp;&nbsp;______&nbsp;<br>|__&nbsp;&nbsp;&nbsp;__|&nbsp;|&nbsp;&nbsp;____|<br>&nbsp;&nbsp;&nbsp;|&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;|__&nbsp;&nbsp;&nbsp;<br>&nbsp;&nbsp;&nbsp;|&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;__|&nbsp;&nbsp;<br>&nbsp;&nbsp;&nbsp;|&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>&nbsp;&nbsp;&nbsp;|_|&nbsp;&nbsp;&nbsp;&nbsp;|_|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>",
+  "Terminal Faker version " + version + "<br>",
+  "By Sherri Wheeler (SyntaxSeed.com)<br>",
+  "Ready. Type 'help' to get started.<br><br>",
+  "题目:<br>",
+  "在当前目录下有一个docs.md文件,请你打印他作为入群验证<br><br>",
+  "terminalfaker内置命令(可键入help查看) : base64dec  base64enc  cat  cd  clear  cow  decrypt  echo  encrypt  hello  help  history  ls  pwd  reboot  rm  touch  version <br><br>",
+];
+
+let bootMessageLines = isZH ? bootMessageLines_zh : bootMessageLines_en;
